725fd3652073d0be1f846e2c6d56281f4087de28
Benjamin Cohen <benjamin.cohen86@gmail.com>
2014-06-19 18:12:55 -0400

    creates steps in building application

--- js/app.js
+++ js/app.js
@@ -15,3 +15,14 @@ $(document).ready(function(){
 });
 
 
+Game = {
+
+}
+
+// Create function newGame () {}
+// create a function randomNumber to create a number 1-100 that is chosen for function newGame  
+// newGame begins when page loads or user clicks New Game Button
+// create function userFeedback using set ranges, 1-10 = extremely hot, 11-20 = warm, 21-50 = cold, 51-100 = freezing
+// the feedback should appear in Div#feedback, replacing Make Your Guess
+// Create function guessHistory and attach to #guessList
+// Create a regular expression for input that only allows them to guess 1-100
